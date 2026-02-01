dc7c7b1c9d914f5625b6f3ae7ad1ef383ee9c819
Jamey Hake <hakedev@Jameys-MacBook-Pro.local>
2026-02-01 13:01:25 -0700

    chore: Update favicon to PNG

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Localhake',
   tagline: 'Homelab tutorials and documentation',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
